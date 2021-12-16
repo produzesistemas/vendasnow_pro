36b2fdeecc0804c6fe4137a3e2b3f4641030c2f8
Eros Pereira <especievil12345@gmail.com>
2021-12-16 18:03:28 -0300

    recycleview em contas a receber

--- app/src/main/java/com/produze/sistemas/vendasnow/vendasnowpremium/ui/adapters/AdapterAccountReceivable.kt
+++ app/src/main/java/com/produze/sistemas/vendasnow/vendasnowpremium/ui/adapters/AdapterAccountReceivable.kt
@@ -1,5 +1,6 @@
 package com.produze.sistemas.vendasnow.vendasnowpremium.ui.adapters
 
+import android.R.attr
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Filter
@@ -17,6 +18,20 @@ import com.produze.sistemas.vendasnow.vendasnowpremium.viewmodel.ViewModelDetail
 import java.text.NumberFormat
 import java.text.SimpleDateFormat
 import java.util.*
+import android.widget.TableLayout
+import android.view.Gravity
+
+import android.R.attr.textSize
+
+import android.util.TypedValue
+
+import android.widget.TextView
+
+
+
+
+
+
 
 class AdapterAccountReceivable (private val lst: List<Sale>, var viewModel: ViewModelAccountReceivable, var viewModelDetailSale: ViewModelDetailSale) :
     RecyclerView.Adapter<AdapterAccountReceivable.RecyclerViewViewHolder>(), Filterable {
@@ -25,12 +40,15 @@ class AdapterAccountReceivable (private val lst: List<Sale>, var viewModel: View
     private var lstFilter: List<Sale> = arrayListOf()
     val nFormat: NumberFormat = NumberFormat.getCurrencyInstance(Locale("pt", "BR"))
     var df = SimpleDateFormat("dd/MM/yyyy")
+    private var mTableLayout: TableLayout? = null
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewViewHolder{
         val binding: CardViewAccountReceivableBinding = DataBindingUtil.inflate(
             LayoutInflater.from(parent.context),
             R.layout.card_view_account_receivable,
             parent,
             false)
+
+        val tv2 = TextView(parent.context)
 
         return RecyclerViewViewHolder(binding)
     }
@@ -47,11 +65,15 @@ class AdapterAccountReceivable (private val lst: List<Sale>, var viewModel: View
         RecyclerView.ViewHolder(binding.root) {
         fun bind(lst: List<Sale>, position: Int) {
 
-            val dt = lst[position].salesDate?.let { it }
-            binding.textViewSalesDate.text = df.format(dt)
+//            val dt = lst[position].salesDate?.let { it }
+//            binding.textViewSalesDate.text = df.format(dt)
             binding.textViewClient.text = lst[position].client?.name
             binding.textViewPayment.text = lst[position].formPayment?.name
-            binding.viewDetail.setBackgroundColor(itemView.getResources().getColor(R.color.green))
+            binding.viewDetail.setBackgroundColor(itemView.getResources().getColor(R.color.purple))
+
+            lst[position].accounts.forEach {
+
+            }
 
             binding.cardViewSale.setOnClickListener {
                 sale = lst[position]
@@ -61,7 +83,7 @@ class AdapterAccountReceivable (private val lst: List<Sale>, var viewModel: View
                 }
             }
 
-            binding.textViewTotal.text = nFormat.format(getTotalSale(lst[position].saleServices.toMutableList(), lst[position].saleProducts.toMutableList()))
+//            binding.textViewTotal.text = nFormat.format(getTotalSale(lst[position].saleServices.toMutableList(), lst[position].saleProducts.toMutableList()))
 
         }
 
